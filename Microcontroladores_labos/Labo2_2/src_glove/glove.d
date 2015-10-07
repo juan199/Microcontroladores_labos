3f33711c5b0cf11c4619507de96e3fa48d8542f1
juan199 <juancho0_8@hotmail.com>
2015-10-07 16:39:30 -0600

    Labo 2 info

---
+++ Microcontroladores_labos/Labo2_2/src_glove/glove.d
@@ -0,0 +1,39 @@
+glove.o: glove.c ../libopencm3/include/libopencm3/stm32/f4/rcc.h \
+ ../libopencm3/include/libopencm3/stm32/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/f4/memorymap.h \
+ ../libopencm3/include/libopencm3/cm3/memorymap.h \
+ ../libopencm3/include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/4.8.2/include/stdint.h \
+ /usr/include/newlib/stdint.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/4.8.2/include/stdbool.h \
+ ../libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ ../libopencm3/include/libopencm3/stm32/f4/gpio.h \
+ ../libopencm3/include/libopencm3/stm32/common/gpio_common_f24.h \
+ ../libopencm3/include/libopencm3/stm32/common/gpio_common_all.h \
+ ../libopencm3/include/libopencm3/stm32/common/gpio_common_f234.h \
+ ../libopencm3/include/libopencm3/stm32/f4/adc.h \
+ ../libopencm3/include/libopencm3/stm32/f4/timer.h \
+ ../libopencm3/include/libopencm3/stm32/common/timer_common_f24.h \
+ ../libopencm3/include/libopencm3/stm32/common/timer_common_all.h \
+ ../libopencm3/include/libopencm3/stm32/f4/nvic.h \
+ ../libopencm3/include/libopencm3/cm3/nvic.h \
+ ../libopencm3/include/libopencm3/dispatch/nvic.h \
+ ../libopencm3-plus/include/libopencm3-plus/newlib/syscall.h \
+ /usr/include/newlib/stdio.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/sys/config.h \
+ /usr/include/newlib/machine/ieeefp.h \
+ /usr/lib/gcc/arm-none-eabi/4.8.2/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/4.8.2/include/stdarg.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/types.h \
+ /usr/include/newlib/machine/types.h /usr/include/newlib/sys/stdio.h \
+ ../libopencm3-plus/include/libopencm3-plus/cdcacm_one_serial/cdcacm.h \
+ ../libopencm3-plus/include/libopencm3-plus/utils/misc.h \
+ ../libopencm3-plus/include/libopencm3-plus/stm32f4discovery/leds.h \
+ ../libopencm3/include/libopencm3/stm32/gpio.h glove.h \
+ /usr/lib/gcc/arm-none-eabi/4.8.2/include-fixed/limits.h \
+ /usr/include/newlib/math.h /usr/include/newlib/string.h \
+ /usr/include/newlib/sys/cdefs.h /usr/include/newlib/sys/string.h
